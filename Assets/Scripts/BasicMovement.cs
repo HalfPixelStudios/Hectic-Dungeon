fa51998a00162074673aaa92be341cf145db71fa
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-03-15 12:10:08 -0400

    basics of movement

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour {
+
+    static float moveThreshold = 0.05f;
+    public GameObject movePoint;
+    [Range(0f, 1f)] public float move_speed;
+    
+    void Start() {
+        movePoint.transform.parent = null;
+    }
+
+    void Update() {
+
+        transform.position = Vector2.MoveTowards(transform.position, movePoint.transform.position, move_speed);
+        if (Vector2.Distance(transform.position, movePoint.transform.position) <= moveThreshold) {
+            movePoint.transform.position += nextMove();
+        }
+    }
+
+    public virtual Vector3 nextMove() {
+        return Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : BasicMovement {
+
+    public override Vector3 nextMove() {
+
+        //Assume keyboard input
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
+            return new Vector3(Input.GetAxisRaw("Horizontal"), 0f,0f);
+        } else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
+            return new Vector3(0f,Input.GetAxisRaw("Vertical"),0f);
+        }
+        return Vector3.zero;
+    }
+}
