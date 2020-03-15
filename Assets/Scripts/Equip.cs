326cc2f3472ff1e943d36eb1c7d9eba8b0f14e56
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-03-15 16:03:22 -0400

    added pickup

---
+++ Assets/Scripts/Equip.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equip : MonoBehaviour
+{
+    private GameObject equipped;
+    void Start()
+    {
+        
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire")&&equipped!=null)
+        {
+            GetComponentInChildren<Equipment>().activate();
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print(true);
+        if (other.gameObject.GetComponentInChildren<Equipment>() != null)
+        {
+            if (equipped != null)
+            {
+                Destroy(equipped);
+            }
+            Transform equipment = other.gameObject.transform.GetChild(0);
+            equipment.SetParent(transform);
+            equipped = equipment.gameObject;
+            Destroy(other.gameObject);
+
+        }
+    }
+}
