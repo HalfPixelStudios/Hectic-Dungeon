3e8bd0aa1015a626747b0f4df6c4d478eb9a311b
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-03-15 17:11:55 -0400

    added empty tiles

--- Assets/Scripts/FloorGenerator.cs
+++ Assets/Scripts/FloorGenerator.cs
@@ -6,21 +6,28 @@ public class FloorGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject tilePrefab;
     [SerializeField] private List<Sprite> tileSprites;
-
+    
 
     // Start is called before the first frame update
     void Start()
     {
 
         var floor = GetComponent<GlobalContainer>().floor;
-        for (float y = -1.5f; y >-15; y--)
+        for (int y = 0; y <14; y++)
         {
-            for (float x = 1.5f; x < 14.5f; x++)
+            for (int x = 0; x < 13; x++)
             {
+                if (Random.Range(0f, 1f)<0.07f)
+                {
+                    continue;
 
-                GameObject g = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity);
+                }
+                
+
+                GameObject g = Instantiate(tilePrefab, new Vector3(1.5f+x, -1.5f-y, 0), Quaternion.identity);
                 g.AddComponent<SpriteRenderer>().sprite = tileSprites[Random.Range(0, 2)];
                 g.transform.SetParent(floor.transform);
+                
             }
             
         }
