3f69ffc3966e81fbd42a4a8320cf50d2f4c5b68a
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-03-15 10:55:34 -0400

    basic map setup

---
+++ Assets/Maps/Tilesets/dungeon_set_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="dungeon_set_2" tilewidth="16" tileheight="16" tilecount="128" columns="8">
+ <image source="../Images/dungeon_set_2.png" width="128" height="256"/>
+</tileset>
