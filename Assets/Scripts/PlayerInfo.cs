e549df031ec3a86baff4f0671041e171503b0145
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-03-15 14:16:06 -0400

    redid input again

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class PlayerInfo : MonoBehaviour {
 
+    public Animator anim;
+    public SpriteRenderer sr;
+
     //Input stuff
-    PlayerInputActions inputActions;
-    public Vector2 moveInput;
+    //PlayerInputActions inputActions;
     [Range(0f, 1f)] public float inputThreshold;
+    //public Vector2 moveInput;
+
 
     void Awake() {
-        inputActions = new PlayerInputActions();
-        inputActions.PlayerControls.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+
+        //inputActions = new PlayerInputActions();
+        //inputActions.PlayerControls.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
     }
 
+    /*
     private void OnEnable() {
         inputActions.Enable();
     }
     private void OnDisable() {
         inputActions.Disable();
     }
-
+    */
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -25,18 +25,28 @@ public class PlayerMovement : MonoBehaviour {
         
         //Get player input
         if (Vector2.Distance(info.gameObject.transform.position, movePoint.transform.position) <= moveThreshold) {
-            Vector3 nextMove = Vector3.zero;
 
-            if (Mathf.Abs(info.moveInput.x) >= info.inputThreshold) {
-                facing = (info.moveInput.x >= 0) ? true : false;
-                nextMove = new Vector3(info.moveInput.x / Mathf.Abs(info.moveInput.x), 0f, 0f);
-            } else if (Mathf.Abs(info.moveInput.y) >= info.inputThreshold) {
-                nextMove = new Vector3(0f, info.moveInput.y / Mathf.Abs(info.moveInput.y), 0f);
-            }
+            //assume keyboard
+            int x = 0; int y = 0;
+            if (Input.GetButtonDown("Up")) { y = 1; }
+            else if (Input.GetButtonDown("Down")) { y = -1; }
+            else if (Input.GetButtonDown("Left")) { x = -1; }
+            else if (Input.GetButtonDown("Right")) { x = 1; }
 
-            movePoint.transform.position += nextMove;
+
+            movePoint.transform.position += new Vector3(x,y,0);
         }
 
+
+        /*
+if (Mathf.Abs(info.moveInput.x) >= info.inputThreshold) {
+    facing = (info.moveInput.x >= 0) ? true : false;
+    info.sr.flipX = !facing; //flip player direction based on input
+    nextMove = new Vector3(info.moveInput.x / Mathf.Abs(info.moveInput.x), 0f, 0f);
+} else if (Mathf.Abs(info.moveInput.y) >= info.inputThreshold) {
+    nextMove = new Vector3(0f, info.moveInput.y / Mathf.Abs(info.moveInput.y), 0f);
+}
+*/
     }
-    
+
 }
