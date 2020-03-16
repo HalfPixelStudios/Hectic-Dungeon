df3e0000adca61ecfbd54157f82059d792621adc
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-03-16 16:08:48 -0400

    fixed enemy on top of one another and Item Spawner

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,6 +7,12 @@ public class EnemyMovement : MonoBehaviour {
     [SerializeField] [Range(0.001f, 1f)] public float move_speed;
     bool facing = true; //1 - facing right, 0 facing left
     public Vector3 movePoint;
+    private EnemyAI _enemyAi;
+
+    private void Start()
+    {
+        _enemyAi = GetComponent<EnemyAI>();
+    }
 
     void Awake()
     {
@@ -18,7 +24,20 @@ public class EnemyMovement : MonoBehaviour {
     }
     public void move()
     {
-        movePoint += GetComponent<EnemyAI>().nextStep;
+        foreach (var enemyMovement in GlobalContainer.global.enemies.GetComponentsInChildren<EnemyMovement>())
+        {
+            if (!Equals(enemyMovement, this))
+            {
+                if (movePoint + _enemyAi.nextStep == enemyMovement.movePoint)
+                {
+                    return;
+                }
+                
+            }
+            
+        }
+        
+        movePoint += _enemyAi.nextStep;
     }
     
 
